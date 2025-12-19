d48f3242042aafcad22f2fed18e8674f3e530fb5
danielgils <danielgils@gmail.com>
2025-12-19 15:14:44 -0500

    build: expose some functions

--- src/data360_ops/__init__.py
+++ src/data360_ops/__init__.py
@@ -1,6 +1,17 @@
 # read version from installed package
 from importlib.metadata import version
-from .github import create_issues_for_dataset
+from .github import create_issues_for_dataset, load_token
+from .one_drive import (
+    get_token_with_username,
+    download_file,
+    list_files
+)
 
 __version__ = version("data360_ops")
-__all__ = ["create_issues_for_dataset"]
+__all__ = [
+    "create_issues_for_dataset",
+    "load_token",
+    "get_token_with_username",
+    "download_file",
+    "list_files"
+]

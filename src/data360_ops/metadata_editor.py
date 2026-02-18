fcec2c0aeb08c06368f120b411ece8ec346c5b40
danielgils <danielgils@gmail.com>
2026-02-18 17:46:32 -0500

    feat(metadata): add function to filter by date

--- src/data360_ops/metadata_editor.py
+++ src/data360_ops/metadata_editor.py
@@ -51,3 +51,40 @@ def get_projects_from_collection(
 	me_projects_df.reset_index(drop=False, inplace=True)
 
 	return me_projects_df
+
+
+def filter_projects_by_changed_date(
+		projects_df: pd.DataFrame,
+		start_date: str|None=None,
+		end_date: str|None=None) -> pd.DataFrame:
+	"""Filter projects dataframe based on a date range for the changed_utc_date column.
+	If start_date and end_date are empty, this function will return projects modified in the last 48 hours.
+
+	The reason for using last 48 hours instead of last 24 hours is to account for any timezone differences and ensure we capture all projects modified "today" in any timezone.
+
+	Args:
+		projects_df (pd.DataFrame): DataFrame containing projects with a 'changed_utc_date' column.
+		start_date (str): Start date in 'YYYY-MM-DD' format.
+		end_date (str): End date in 'YYYY-MM-DD' format.
+
+	Returns:
+		pd.DataFrame: Filtered DataFrame containing projects where 'changed_utc_date' is between the specified start and end dates.
+	"""
+	# Set start and end dates as timestamps with timezone
+	today = pd.Timestamp.today()
+	if start_date is None:
+		# Set start_date to 48 hours ago to account for timezone differences and ensure we capture all projects modified "today" in any timezone
+		starting_date = pd.Timestamp((today - pd.Timedelta(days=1)).normalize(), tz='UTC')
+	else:
+		starting_date = pd.Timestamp(start_date, tz='UTC')
+
+	if end_date is None:
+		ending_date = pd.Timestamp((today + pd.Timedelta(days=1)).normalize(), tz='UTC')
+	else:
+		ending_date = pd.Timestamp(end_date, tz='UTC')
+
+	filtered_projects_range = projects_df[
+		(projects_df['changed_utc_date'] >= starting_date) & 
+		(projects_df['changed_utc_date'] <= ending_date)].copy()
+	
+	return filtered_projects_range
